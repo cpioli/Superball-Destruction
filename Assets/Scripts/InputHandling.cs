419d559122c681a7fc271cecd5f7c381e8bd2f9f
Chris Pioli <christopher.pioli@gmail.com>
2018-04-25 13:41:36 -0700

    Updated Input Handling

--- Assets/Scripts/InputHandling.cs
+++ Assets/Scripts/InputHandling.cs
@@ -14,12 +14,12 @@ public class InputHandling : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-	    if(Input.GetKeyUp(KeyCode.UpArrow))
+	    if(Input.GetKeyUp(KeyCode.UpArrow) && Time.timeScale < 100f)
         {
             Time.timeScale += .1f;
             currentTimeSpeed = Time.timeScale;
         }
-        if (Input.GetKeyUp(KeyCode.DownArrow))
+        if (Input.GetKeyUp(KeyCode.DownArrow) && Time.timeScale >= 0f)
         {
             Time.timeScale -= .1f;
             currentTimeSpeed = Time.timeScale;
