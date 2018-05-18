0001513b7f3e469711ef196ae9bbaf4d807349cb
Chris Pioli <christopher.pioli@gmail.com>
2018-05-18 12:55:04 -0700

    ConeCastCollector Added!
    This object stores all the collideable objects the ball can hit and
    maintain force. It's for the ball's AI.

---
+++ Assets/ConeCastColliderCollector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+This object is a container that stores every GameObject the collision cone connects with
+ */
+public class ConeCastColliderCollector : MonoBehaviour {
+
+
+    private List<Collider> colliderContainer;
+
+	// Use this for initialization
+	void Start () {
+        colliderContainer = new List<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ClearColliderContainer()
+    {
+        colliderContainer.Clear();
+    }
+
+    public List<Collider> GetColliderContainer()
+    {
+        return colliderContainer;
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        colliderContainer.Add(collision.collider);
+    }
+}
