a7b81f54bd0a8b3c6c59dad2bb0928a9603095eb
Chris Pioli <christopher.pioli@gmail.com>
2018-05-29 17:05:24 -0700

    Velocity-halting code works
    I also turned off the gravity so when gravity is turned back on, the
    ball won't fall.

--- Assets/Scripts/ReaimingBehavior.cs
+++ Assets/Scripts/ReaimingBehavior.cs
@@ -4,33 +4,11 @@ using UnityEngine;
 
 public class ReaimingBehavior : MonoBehaviour {
 
-    public float rotationSpeed = 0.5f;
-    GameObject camObject;
-
-    public float sensitivityX = 15F;
-    public float sensitivityY = 15F;
-
-    public float minimumX = -90F;
-    public float maximumX = 90F;
-
-    public float minimumY = -60F;
-    public float maximumY = 60F;
-
-    float rotationX = 0F;
-    float rotationY = 0F;
-
-    private List<float> rotArrayX = new List<float>();
-    private float rotAverageX = 0F;
-
-    private List<float> rotArrayY = new List<float>();
-    private float rotAverageY = 0F;
-
-    public float frameCounter = 20;
-
     private float timeRemaining; //time left until the 
     private Vector3 ballPosition, ballVelocity;
-    private SuperballBehavior sbBehavior;
     private GameObject roomCamera; //we need the GO for movement and aiming purposes
+    private Rigidbody superballRBody;
+    private SuperballBehavior sbBehavior;
 
     public float timeUntilLaunch; //how long can they hold onto the ball before it launches?
 
@@ -38,8 +16,9 @@ public class ReaimingBehavior : MonoBehaviour {
 	void Start () {
         timeRemaining = 0f;
         ballPosition = Vector3.zero;
-        sbBehavior = GameObject.Find("Sphere").GetComponent<SuperballBehavior>();
         roomCamera = GameObject.Find("RoomCamera");
+        sbBehavior = GameObject.Find("Sphere").GetComponent<SuperballBehavior>();
+        superballRBody = GameObject.Find("Sphere").GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -57,8 +36,16 @@ public class ReaimingBehavior : MonoBehaviour {
     {
         ballPosition = GameObject.Find("Sphere").transform.position;
         ballVelocity = GameObject.Find("Sphere").GetComponent<Rigidbody>().velocity;
-        sbBehavior.ballState = SuperballBehavior.SuperBallState.ATREST;
+        HaltSuperballMovement();
+
         PositionAndOrientCamera(collision);
+    }
+
+    private void HaltSuperballMovement()
+    {
+        sbBehavior.ballState = SuperballBehavior.SuperBallState.DEAD;
+        superballRBody.velocity = Vector3.zero;
+        superballRBody.angularVelocity = Vector3.zero;
     }
 
     private void PositionAndOrientCamera(Collision collision)
