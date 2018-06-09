9e63aa4b3f5b87898052afd77df6fffdd05fb5c3
Chris Pioli <christopher.pioli@gmail.com>
2018-06-08 17:30:26 -0700

    Initial UI components created
    Currently inactive in the hierarchy.

---
+++ Assets/Scripts/UIStartup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenusControl : MonoBehaviour {
+
+    public Canvas StartMenu;
+    public Canvas InGameHUD;
+    public Canvas PauseMenu;
+    public Canvas PointTally;
+
+	// Use this for initialization
+	void Awake () {
+        StartMenu.gameObject.SetActive(false);
+        InGameHUD.gameObject.SetActive(false);
+        PauseMenu.gameObject.SetActive(false);
+        PointTally.gameObject.SetActive(false);
+	}
+
+}
