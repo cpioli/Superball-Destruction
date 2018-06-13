bf0db0e73bb5ac8849d2bbcdaa8175ef9e7035e2
Chris Pioli <christopher.pioli@gmail.com>
2018-06-12 17:23:27 -0700

    Bug fix: bowls not disappearing properly
    First the bowls disappeared from one side of the shelf en masse when hit
    with the ball. By determining whether the collider is a mesh or box type
    I fixed that.
    
    However the mesh colliders wouldn't disappear when the ball hit it, so I
    had to traverse the transforms in the hierarchy to find the Mesh
    Collider and disable it.

--- Assets/Scripts/MirrorBehavior.cs
+++ Assets/Scripts/MirrorBehavior.cs
@@ -9,6 +9,11 @@ public class MirrorBehavior : MonoBehaviour {
         // magnitude of a vector is its length, and it tells us how fast the ball is going
 
         this.GetComponent<MeshRenderer>().enabled = false;
-        this.GetComponent<BoxCollider>().enabled = false;
+        if(GetComponent<BoxCollider>() != null)
+            this.GetComponent<BoxCollider>().enabled = false;
+        else
+        {
+            this.transform.GetChild(0).gameObject.GetComponent<MeshCollider>().enabled = false;
+        }
     }
 }
