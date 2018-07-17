dbb5de0f08100bf98c97223cd86887be0b90c09c
Chris Pioli <christopher.pioli@gmail.com>
2018-07-17 16:13:52 -0700

    Adding Camera AI
    A script to handle camera movement has been added and it's been given
    basic ball-tracking behavior.

---
+++ Assets/Scripts/CameraAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAI : MonoBehaviour {
+
+    private GameObject ball;
+
+    void Start () {
+        ball = GameObject.Find("Sphere");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if(ball.GetComponent<SuperballBehavior>().ballState == SuperballBehavior.SuperBallState.LIVE)
+            transform.LookAt(ball.transform.position);
+	}
+
+
+}
