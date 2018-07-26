7cbb4e6f4b658a5016a9f3a587bfaf0c5d856955
Chris Pioli <christopher.pioli@gmail.com>
2018-07-26 13:28:05 -0700

    Minor Cleanup
    Cleaning up some code. Removing unused methods, removing commented out
    lines, adding tons of comments to indicate what the algorithm does.

--- Assets/Scripts/SuperballAI.cs
+++ Assets/Scripts/SuperballAI.cs
@@ -6,7 +6,9 @@ public class SuperballAI : MonoBehaviour {
 
     public float fieldOfView;
     public float maxMoveInDegrees;
+
     private float radius;
+    private float debugLineDuration = 3f;
     private Vector3 location, newDirection;
     private Collider[] colliders;
     private RaycastHit hitInfo;
@@ -17,14 +19,6 @@ public class SuperballAI : MonoBehaviour {
     {
         sphereCollider = GetComponent<SphereCollider>();
         radius = sphereCollider.radius * sphereCollider.transform.localScale.x;
-        Time.timeScale = 0.5f;
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-//#if UNITY_EDITOR
-//        UnityEditor.EditorApplication.isPaused = true;
-//#endif
     }
 
     void OnCollisionExit(Collision collision)
@@ -34,12 +28,10 @@ public class SuperballAI : MonoBehaviour {
         FindNearestBreakableObject();
         superballAIData.newDirection = (superballAIData.newCollisionPos - location) *superballAIData.physicsDirection.magnitude;
         SuperballAdjustment();
-
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPaused = true;
-#endif
     }
 
+    //This finds the nearest breakable object and stores it in superballAIData
+    //There is no guarantee the object will fall within the range of the superball's movement.
     private void FindNearestBreakableObject()
     {
         superballAIData.newCollisionPos = Vector3.zero;
@@ -60,7 +52,10 @@ public class SuperballAI : MonoBehaviour {
             currentColliderBestPos = colliders[i].ClosestPoint(location);
             Physics.SphereCast(location, radius, currentColliderBestPos - location, out hitInfo, 1 << 8);
             if (hitInfo.collider != colliders[i]) { continue; }
-
+            //2b. measure the angle created by 2 vectors: 
+            //    i. the direction Unity physics calculates the ball to move,
+            //    ii. the direction the ball should move if it wants to hit
+            //        the current object we're iterating through
             currentCollidersAngle = Vector3.Angle(superballAIData.physicsDirection, currentColliderBestPos - location);
             if (currentCollidersAngle < superballAIData.degrees)
             {
@@ -90,23 +85,28 @@ public class SuperballAI : MonoBehaviour {
 
         Quaternion from = Quaternion.LookRotation(superballAIData.physicsDirection, upVector);
         Quaternion to = Quaternion.LookRotation(superballAIData.newDirection, upVector);
+
+        //if the ball can move to meet the object we're interested in, do it
         if (superballAIData.degrees <= maxMoveInDegrees)
         {
             newVelocity = Quaternion.RotateTowards(from, to, superballAIData.degrees) * Vector3.forward;
         }
+        //if the ball can't reach the nearest object, rotate it the max degrees so it can at least get CLOSE to it
         else
         {
             newVelocity = Quaternion.RotateTowards(from, to, maxMoveInDegrees) * Vector3.forward;
         }
+        
         this.GetComponent<Rigidbody>().velocity = newVelocity.normalized * superballAIData.physicsDirection.magnitude;
 
-        print("oldVelocity: " + superballAIData.physicsDirection.magnitude 
-            + " * " + superballAIData.physicsDirection.normalized + " = " + superballAIData.physicsDirection);
-        print("newVelocity: " + GetComponent<Rigidbody>().velocity.magnitude 
-            + " * " + GetComponent<Rigidbody>().velocity.normalized + " = " + GetComponent<Rigidbody>().velocity);
-        Debug.DrawLine(transform.position, transform.position + superballAIData.physicsDirection, Color.yellow, 480f);
-        Debug.DrawLine(transform.position, superballAIData.newCollisionPos, Color.red, 480f);
-        Debug.DrawLine(transform.position, transform.position + this.GetComponent<Rigidbody>().velocity, Color.green, 480f);
+        //the line indicating where Unity's physics system wanted us to move
+        Debug.DrawLine(transform.position, transform.position + superballAIData.physicsDirection, Color.blue, debugLineDuration);
+
+        //the line indicating which collision our AI wanted to hit
+        Debug.DrawLine(transform.position, superballAIData.newCollisionPos, Color.red, debugLineDuration);
+
+        //the line indicating the actual direction the ball will move
+        Debug.DrawLine(transform.position, transform.position + this.GetComponent<Rigidbody>().velocity, Color.green, debugLineDuration);
     }
 
     struct SuperballAIData
