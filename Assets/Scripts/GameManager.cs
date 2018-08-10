86c7ad5de8a40d213418ac56fd3b889255c38b2a
Chris Pioli <christopher.pioli@gmail.com>
2018-08-09 17:28:05 -0700

    Fixed Ball AI's configuration
    Ball AI's configuration has a non-zero value in maxMoveDegrees, so it's
    working again.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -122,11 +122,25 @@ public class GameManager : MonoBehaviour, IGameEventHandler {
         {
             GameObject go = destroyedObjects.Pop();
             go.GetComponent<MeshRenderer>().enabled = true;
-            if (GetComponent<BoxCollider>() != null)
-                this.GetComponent<BoxCollider>().enabled = true;
+            if (go.GetComponent<BoxCollider>() != null)
+                go.GetComponent<BoxCollider>().enabled = true;
             else
             {
-                this.transform.GetChild(0).gameObject.GetComponent<MeshCollider>().enabled = true;
+                try
+                {
+                    GameObject go1 = go.transform.GetChild(0).gameObject;
+                    
+                } catch(UnityException e)
+                {
+                    string s = go.name;
+                    while(go.transform.parent != null)
+                    {
+                        go = go.transform.parent.gameObject;
+                        s += "-" + go.name;
+                    }
+                    Debug.LogError("Having a problem with object " + s);
+                    print(e.Message);
+                }
             }
         }
     }
