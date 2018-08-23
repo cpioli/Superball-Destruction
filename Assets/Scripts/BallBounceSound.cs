742c8e847ede75079cfa4019820b6a37c15fc505
Chris Pioli <christopher.pioli@gmail.com>
2018-08-23 16:52:26 -0700

    Added breaking sounds and bouncing sounds
    Added 3 tags to tell the ball what sound it needs to use.

---
+++ Assets/Scripts/BallBounceSound.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBounceSound : MonoBehaviour {
+
+    public AudioClip bounceAgainstTable;
+    public AudioClip bounceAgainstPlate;
+    public AudioClip bounceAgainstWalls;
+
+    private AudioSource source;
+    private float lowPitchRange = .75f;
+    private float highPitchRange = 1.5f;
+    private float velocityToVolume = 0.2f;
+
+    private string breakable = "Breakable";
+    private string unbreakable = "Unbreakable";
+    private string wall = "Wall";
+
+	void Awake () {
+        source = GetComponent<AudioSource>();
+	}
+
+    private void OnCollisionEnter(Collision col)
+    {
+        source.pitch = Random.Range(lowPitchRange, highPitchRange);
+        float hitVol = col.relativeVelocity.magnitude * velocityToVolume;
+        print("gameobject type: " + col.gameObject.tag + " ; hitVol: " + hitVol);
+        if (col.gameObject.CompareTag(breakable)) //is it breakable
+        {
+            source.PlayOneShot(bounceAgainstPlate, hitVol);
+        }
+        else if (col.gameObject.CompareTag(unbreakable))
+        {
+            source.PlayOneShot(bounceAgainstTable, hitVol);
+        }
+        else if (col.gameObject.CompareTag(wall))
+        {
+            source.PlayOneShot(bounceAgainstWalls, hitVol);
+        }
+    }
+}
